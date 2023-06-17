e7e6f26f25151aaae6d0d6f51480632af8c5df64
NightFeather <leolee50910@gmail.com>
2023-06-17 21:44:33 +0800

    Remove poise

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,8 @@
-mod commands;
 mod oauth;
 
 use oauth::TwitterClient;
-use poise::serenity_prelude as serenity;
 use dotenv::dotenv;
-use std::{collections::HashMap, sync::Mutex, time::Duration, env, io, rc::Rc};
-
-// Types used by all command functions
-type Error = Box<dyn std::error::Error + Send + Sync>;
-type Context<'a> = poise::Context<'a, Data, Error>;
-
-// Custom user data passed to all command functions
-pub struct Data {
-    votes: Mutex<HashMap<String, u32>>,
-}
+use std::{env, io};
 
 #[tokio::main]
 async fn main() {
@@ -28,7 +17,7 @@ async fn main() {
     );
     println!("{:?}", auth.get_authorization_url().await);
 
-    let mut input_text = String::new();
+    let mut input_text: String = String::new();
     io::stdin()
         .read_line(&mut input_text)
         .expect("failed to read from stdin");
@@ -40,94 +29,3 @@ async fn main() {
     let author_id: String = auth.get_author_id("1669464988161908738").await;
     auth.follow(&author_id).await;
 }
-
-// async fn on_error(error: poise::FrameworkError<'_, Data, Error>) {
-//     // This is our custom error handler
-//     // They are many errors that can occur, so we only handle the ones we want to customize
-//     // and forward the rest to the default handler
-//     match error {
-//         poise::FrameworkError::Setup { error, .. } => panic!("Failed to start bot: {:?}", error),
-//         poise::FrameworkError::Command { error, ctx } => {
-//             println!("Error in command `{}`: {:?}", ctx.command().name, error,);
-//         }
-//         error => {
-//             if let Err(e) = poise::builtins::on_error(error).await {
-//                 println!("Error while handling error: {}", e)
-//             }
-//         }
-//     }
-// }
-
-// #[tokio::main]
-// async fn main() {
-//     env_logger::init();
-
-//     // FrameworkOptions contains all of poise's configuration option in one struct
-//     // Every option can be omitted to use its default value
-//     let options = poise::FrameworkOptions {
-//         commands: vec![commands::help(), commands::vote(), commands::getvotes()],
-//         prefix_options: poise::PrefixFrameworkOptions {
-//             prefix: Some("~".into()),
-//             edit_tracker: Some(poise::EditTracker::for_timespan(Duration::from_secs(3600))),
-//             additional_prefixes: vec![
-//                 poise::Prefix::Literal("hey bot"),
-//                 poise::Prefix::Literal("hey bot,"),
-//             ],
-//             ..Default::default()
-//         },
-//         /// The global error handler for all error cases that may occur
-//         on_error: |error| Box::pin(on_error(error)),
-//         /// This code is run before every command
-//         pre_command: |ctx| {
-//             Box::pin(async move {
-//                 println!("Executing command {}...", ctx.command().qualified_name);
-//             })
-//         },
-//         /// This code is run after a command if it was successful (returned Ok)
-//         post_command: |ctx| {
-//             Box::pin(async move {
-//                 println!("Executed command {}!", ctx.command().qualified_name);
-//             })
-//         },
-//         /// Every command invocation must pass this check to continue execution
-//         command_check: Some(|ctx| {
-//             Box::pin(async move {
-//                 if ctx.author().id == 123456789 {
-//                     return Ok(false);
-//                 }
-//                 Ok(true)
-//             })
-//         }),
-//         /// Enforce command checks even for owners (enforced by default)
-//         /// Set to true to bypass checks, which is useful for testing
-//         skip_checks_for_owners: false,
-//         event_handler: |_ctx, event, _framework, _data| {
-//             Box::pin(async move {
-//                 println!("Got an event in event handler: {:?}", event.name());
-//                 Ok(())
-//             })
-//         },
-//         ..Default::default()
-//     };
-
-//     poise::Framework::builder()
-//         .token(
-//             "***",
-//         )
-//         .setup(move |ctx, _ready, framework| {
-//             Box::pin(async move {
-//                 println!("Logged in as {}", _ready.user.name);
-//                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-//                 Ok(Data {
-//                     votes: Mutex::new(HashMap::new()),
-//                 })
-//             })
-//         })
-//         .options(options)
-//         .intents(
-//             serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT | serenity::GatewayIntents::GUILD_MEMBERS,
-//         )
-//         .run()
-//         .await
-//         .unwrap();
-// }
